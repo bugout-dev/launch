c86d819444fb817d5771fceeb4cce8f91423a99e
kompotkot <zenitsws@gmail.com>
2021-10-20 13:37:25 +0000

    Proper sed config change for heimdall

--- mainnet-v1/sentry/sentry/heimdall/setup.sh
+++ mainnet-v1/sentry/sentry/heimdall/setup.sh
@@ -11,5 +11,11 @@ heimdalld init
 # copy node directories to home directories
 cp -rf $NODE_DIR/heimdall/config/genesis.json $HEIMDALL_HOME/config/
 
+# add seeds
+SEEDS_LINE='seeds = "f4f605d60b8ffaaf15240564e58a81103510631c@159.203.9.164:26656,4fb1bc820088764a564d4f66bba1963d47d82329@44.232.55.71:26656"'
+sed -i "s|^seeds =.*|$SEEDS_LINE|" $HEIMDALL_HOME/config/config.toml
+ETHEREUM_RPC_URI="http://127.0.0.1:18370"
+sed -i "s|^eth_rpc_url =.*|$ETHEREUM_RPC_URI|" $HEIMDALL_HOME/config/heimdall-config.toml
+
 # copy .heimdal to mount directory
 cp -rf $HEIMDALL_HOME $MOUNT_DATA_DIR
