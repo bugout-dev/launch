5e640b226718a6b9f2a2a83635cd387ecc43c41f
kompotkot <zenitsws@gmail.com>
2022-01-19 20:25:00 +0000

    Proper place for incorrect bor start logging to mount dir

--- mainnet-v1/sentry/sentry/bor/start.sh
+++ mainnet-v1/sentry/sentry/bor/start.sh
@@ -2,7 +2,9 @@
 
 set -x #echo on
 
+. $HOME/launch/mainnet-v1/variables.sh
+
 MESSAGE="Bor start moved to bor.sevice, do not call this file!"
 echo "${MESSAGE}"
 TIMESTAMP=$(date "+%H:%M:%S %d-%m-%Y")
-echo "${TIMESTAMP} ${MESSAGE} >> /home/ubuntu/bor-start-error.log"
+echo "${TIMESTAMP} ${MESSAGE} >> ${MOUNT_DATA_DIR}/bor-start-incorrect.log"
